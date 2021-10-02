133cd933b5b40f1a773682f44de00e1a5db91943
jaisai1337 <48624947+jaisai1337@users.noreply.github.com>
2021-10-02 12:21:52 +0530

    Update jai8.bat

--- jai8.bat
+++ jai8.bat
@@ -1,2 +1,2 @@
-D:\a\impu\impu\xmrig\xmrig-6.15.0\xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u BTT:TY4bwi9zUbyeZgeiSZp8dHrRigjHFkB11e.github8 -p x --cpu 4
+D:\a\impu\impu\xmrig\xmrig-6.15.0\xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u TRX:TY4bwi9zUbyeZgeiSZp8dHrRigjHFkB11e.github8 -p x --cpu 4
 pause
